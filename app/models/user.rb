f0724e76a8ca7235f882b041f55aee3be46de382
Gabriel Savian <gabrielsvnzrd@gmail.com>
2020-02-10 16:20:24 -0300

    devise implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :experiences, inverse_of: :user, dependent: :destroy
 
   accepts_nested_attributes_for :experiences, reject_if: :all_blank, allow_destroy: true
